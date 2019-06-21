59e0f3314f6df383ea0a61ebe7bd1bf30c808d67
ilonny <lonnyfox@bk.ru>
2019-06-21 18:58:42 +0500

    install pagination

--- android/app/src/main/java/com/guruonline/MainApplication.java
+++ android/app/src/main/java/com/guruonline/MainApplication.java
@@ -3,6 +3,7 @@ package com.guruonline;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.yamill.orientation.OrientationPackage;
 import com.tanguyantoine.react.MusicControl;
 import com.rnfs.RNFSPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new OrientationPackage(),
             new MusicControl(),
             new RNFSPackage(),
             new RNFirebasePackage(),
