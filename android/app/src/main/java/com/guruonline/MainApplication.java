e538a2057ca62415194231fc2ce96587514373de
ilonny <lonnyfox@bk.ru>
2019-06-21 18:29:33 +0500

    install epub and downloader

--- android/app/src/main/java/com/guruonline/MainApplication.java
+++ android/app/src/main/java/com/guruonline/MainApplication.java
@@ -3,6 +3,8 @@ package com.guruonline;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.eko.RNBackgroundDownloaderPackage;
+import com.futurepress.staticserver.FPStaticServerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNBackgroundDownloaderPackage(),
+            new FPStaticServerPackage()
       );
     }
 
